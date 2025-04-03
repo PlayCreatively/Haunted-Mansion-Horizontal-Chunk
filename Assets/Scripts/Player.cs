16fc25f05443fd935b11fb0a04ee4b4c9fdb9adb
Alexander Freyr <alexander-freyr.torgeirsson.3043@student.uu.se>
2025-04-03 18:39:51 +0200

    2p movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,32 +2,41 @@ using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(Rigidbody)), SelectionBase]
 public class Player : MonoBehaviour
 {
-    // new input system
-    InputSystem_Actions inputActions;
-
+    PlayerInput playerInput;
     Rigidbody rb;
+
+    Vector2 moveInput;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        inputActions = new InputSystem_Actions();
-        inputActions.Enable();
-        inputActions.Player.Jump.performed += Jump;
+        playerInput = GetComponent<PlayerInput>();
+
+        playerInput.SwitchCurrentControlScheme(
+            playerInput.defaultControlScheme, // or whatever your second scheme is actually called
+            Keyboard.current
+        );
+
+        playerInput.actions["Move"].performed += ctx => moveInput = ctx.ReadValue<Vector2>();
+        playerInput.actions["Move"].canceled += ctx => moveInput = Vector2.zero;
+
+        playerInput.actions.FindAction("Jump").performed += Jump;
+
     }
 
     void Jump(InputAction.CallbackContext context)
     {
         Debug.Log("Jump");
-        rb.linearVelocity = new Vector3(0, GameSettings.Instance.playerJumpForce, 0);
+        rb.linearVelocity += new Vector3(0, GameSettings.Instance.playerJumpForce, 0);
     }
 
     void FixedUpdate()
     {
-        Vector2 input = inputActions.Player.Move.ReadValue<Vector2>();
-        Vector3 deltaMove = Quaternion.AngleAxis(45, Vector3.up) * new Vector3(input.x, 0, input.y) * GameSettings.Instance.playerSpeed;
+        Vector3 deltaMove = Quaternion.AngleAxis(45, Vector3.up) * new Vector3(moveInput.x, 0, moveInput.y) * GameSettings.Instance.playerSpeed;
+        deltaMove.y = rb.linearVelocity.y;
         rb.linearVelocity = deltaMove;
         //rb.rotation = Quaternion.LookRotation(new Vector3(input.x, 0, input.y));
     }

411661e9c909d5c20015c4ad22a4d33f0e443201
Alexander Freyr <alexander-freyr.torgeirsson.3043@student.uu.se>
2025-04-03 22:31:49 +0200

    only jump if grounded

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,6 +9,7 @@ public class Player : MonoBehaviour
     Rigidbody rb;
 
     Vector2 moveInput;
+    bool grounded;
 
     void Start()
     {
@@ -16,27 +17,37 @@ public class Player : MonoBehaviour
         playerInput = GetComponent<PlayerInput>();
 
         playerInput.SwitchCurrentControlScheme(
-            playerInput.defaultControlScheme, // or whatever your second scheme is actually called
+            playerInput.defaultControlScheme,
             Keyboard.current
         );
 
         playerInput.actions["Move"].performed += ctx => moveInput = ctx.ReadValue<Vector2>();
         playerInput.actions["Move"].canceled += ctx => moveInput = Vector2.zero;
-
-        playerInput.actions.FindAction("Jump").performed += Jump;
-
     }
 
-    void Jump(InputAction.CallbackContext context)
+    void Jump()
     {
         rb.linearVelocity += new Vector3(0, GameSettings.Instance.playerJumpForce, 0);
+        grounded = false;
     }
 
     void FixedUpdate()
     {
+        if (grounded && playerInput.actions.FindAction("Jump").IsPressed())
+            Jump();
+
         Vector3 deltaMove = Quaternion.AngleAxis(45, Vector3.up) * new Vector3(moveInput.x, 0, moveInput.y) * GameSettings.Instance.playerSpeed;
         deltaMove.y = rb.linearVelocity.y;
         rb.linearVelocity = deltaMove;
         //rb.rotation = Quaternion.LookRotation(new Vector3(input.x, 0, input.y));
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        grounded = false;
+        foreach (var contact in collision.contacts)
+        {
+            grounded |= contact.normal.y > 0;
+        }
     }
 }

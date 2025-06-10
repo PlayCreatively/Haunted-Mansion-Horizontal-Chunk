be9afb2d38cf3350d8431a70ff4bb13ca9437de8
Alexander Freyr <alexander-freyr.torgeirsson.3043@student.uu.se>
2025-06-10 15:42:55 +0200

    zoom out + same floor color between menu and game

--- Assets/Scripts/DynamicCamera.cs
+++ Assets/Scripts/DynamicCamera.cs
@@ -98,8 +98,9 @@ public class DynamicCamera : MonoBehaviour
 
             Quaternion targetRotation = Quaternion.Euler(45, 45, 0);
             
-            transform.SetPositionAndRotation(Vector3.Lerp(transform.position, targetPos, _smoothing), Quaternion.Lerp(transform.rotation, targetRotation, _smoothing * .15f));
-            mainCamera.orthographicSize = Mathf.Lerp(mainCamera.orthographicSize, targetZoom, _smoothing * .5f);
+            float deltaTime = Time.deltaTime * 60f;
+            transform.SetPositionAndRotation(Vector3.Lerp(transform.position, targetPos, _smoothing * deltaTime), Quaternion.Lerp(transform.rotation, targetRotation, _smoothing * .3f * deltaTime));
+            mainCamera.orthographicSize = Mathf.Lerp(mainCamera.orthographicSize, targetZoom, _smoothing * .5f * deltaTime);
 
             return;
         }
